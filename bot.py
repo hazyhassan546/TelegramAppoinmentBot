8c0bf8cc9a4a89d3b7928b6b0ac3328ac336e543
Hassan Abbas <hazzyhassan888@gmail.com>
2021-08-04 21:31:48 +0500

    changing python3 to python in Procfile

--- bot.py
+++ bot.py
@@ -56,8 +56,9 @@ def main():
     dispatcher.add_error_handler(error)
     updater.start_webhook(listen="0.0.0.0",
                           port=int(PORT),
-                          url_path=keys.API_KEY)
-    updater.bot.setWebhook('https://gentle-cove-69794.herokuapp.com/' + keys.API_KEY)
+                          url_path=keys.API_KEY,
+                          webhook_url=('https://gentle-cove-69794.herokuapp.com/' + keys.API_KEY)
+                          )
     updater.idle()
 
 
